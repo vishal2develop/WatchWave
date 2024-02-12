79525ce9581eede2d51f132f0f857f1f79c1cdba
Vishal Borana <vishal2develop@gmail.com>
2024-02-13 00:31:23 +0530

    Bulk loading images & fonts - without hooks

--- App.js
+++ App.js
@@ -5,34 +5,38 @@ import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 
-// Keep the splash screen visible while we fetch resources
+const loadFonts = (fonts) =>
+  fonts.map(async (font) => await Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
   const [ready, setReady] = useState(false);
 
-  // Effect to handle app readiness and hide splash screen
-  // useEffect(() => {
-  //   const handleFinish = async () => {
-  //     // Set the app as ready
-  //     setReady(true);
-  //     // Hide the splash screen when the app is ready
-  //     await SplashScreen.hideAsync();
-  //   };
-
-  //   handleFinish();
-  // }, [ready]);
-
   useEffect(() => {
     async function prepare() {
+      if (!ready) {
+        SplashScreen.preventAutoHideAsync();
+      }
       try {
         // Pre-load fonts & images, make any API calls you need to do here
-        await Font.loadAsync(Ionicons.font);
-        // use Asset to load images from local
-        await Asset.loadAsync(require("./dreamhouse-logo.png"));
-        // Use prefetch to fetch images that are not on your local system
-        await Image.prefetch(
-          "https://snack-web-player.s3.us-west-1.amazonaws.com/v2/49/assets/src/react-native-logo.png"
-        );
+        // Loading fonts in bulk
+        const fonts = loadFonts([Ionicons.font]);
+        console.log("fonts:", fonts);
+        // Loading both local & remote assets in bulk
+        const images = loadImages([
+          require("./dreamhouse-logo.png"),
+          "https://snack-web-player.s3.us-west-1.amazonaws.com/v2/49/assets/src/react-native-logo.png",
+        ]);
+        await Promise.all([...fonts, ...images]);
+
         // Artificially delay for two seconds to simulate a slow loading
         // experience. Please remove this if you copy and paste the code!
         await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -41,19 +45,13 @@ export default function App() {
       } finally {
         // Tell the application to render
         setReady(true);
+        // hide splash screen after timeout
+        SplashScreen.hideAsync();
       }
     }
 
     prepare();
   }, []);
-
-  // Effect to prevent auto-hiding of splash screen initially
-  useEffect(() => {
-    // Prevent auto-hiding of splash screen when the component mounts
-    if (!ready) {
-      SplashScreen.preventAutoHideAsync();
-    }
-  }, []); // Run the effect only once when the component mounts
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
