53da58c2179c387ff4f0d95b4da8485a568f6e3f
Vishal Borana <vishal2develop@gmail.com>
2024-02-13 00:42:31 +0530

    using font & asset hooks for loading. Note - If you need custom loading logic, it is better to go with useEffect & custom functions

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Text, View, Image } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 
 const loadFonts = (fonts) =>
@@ -19,39 +19,18 @@ const loadImages = (images) =>
 
 export default function App() {
   const [ready, setReady] = useState(false);
+  const [assets] = useAssets([
+    require("./dreamhouse-logo.png"),
+    "https://snack-web-player.s3.us-west-1.amazonaws.com/v2/49/assets/src/react-native-logo.png",
+  ]);
 
-  useEffect(() => {
-    async function prepare() {
-      if (!ready) {
-        SplashScreen.preventAutoHideAsync();
-      }
-      try {
-        // Pre-load fonts & images, make any API calls you need to do here
-        // Loading fonts in bulk
-        const fonts = loadFonts([Ionicons.font]);
-        console.log("fonts:", fonts);
-        // Loading both local & remote assets in bulk
-        const images = loadImages([
-          require("./dreamhouse-logo.png"),
-          "https://snack-web-player.s3.us-west-1.amazonaws.com/v2/49/assets/src/react-native-logo.png",
-        ]);
-        await Promise.all([...fonts, ...images]);
+  const [fonts] = Font.useFonts(Ionicons.font);
 
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setReady(true);
-        // hide splash screen after timeout
-        SplashScreen.hideAsync();
-      }
-    }
-
-    prepare();
-  }, []);
+  if (!assets || !fonts) {
+    SplashScreen.preventAutoHideAsync();
+  } else {
+    SplashScreen.hideAsync();
+  }
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
