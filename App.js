58944c043f71272409545c4e290a5801ceceb1da
Vishal Borana <vishal2develop@gmail.com>
2024-02-12 23:31:39 +0530

    loading single assets & fonts

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
 
 // Keep the splash screen visible while we fetch resources
 
@@ -8,15 +11,41 @@ export default function App() {
   const [ready, setReady] = useState(false);
 
   // Effect to handle app readiness and hide splash screen
+  // useEffect(() => {
+  //   const handleFinish = async () => {
+  //     // Set the app as ready
+  //     setReady(true);
+  //     // Hide the splash screen when the app is ready
+  //     await SplashScreen.hideAsync();
+  //   };
+
+  //   handleFinish();
+  // }, [ready]);
+
   useEffect(() => {
-    const handleFinish = async () => {
-      // Set the app as ready
-      setReady(true);
-      // Hide the splash screen when the app is ready
-      await SplashScreen.hideAsync();
-    };
-    handleFinish();
-  }, [ready]);
+    async function prepare() {
+      try {
+        // Pre-load fonts & images, make any API calls you need to do here
+        await Font.loadAsync(Ionicons.font);
+        // use Asset to load images from local
+        await Asset.loadAsync(require("./dreamhouse-logo.png"));
+        // Use prefetch to fetch images that are not on your local system
+        await Image.prefetch(
+          "https://snack-web-player.s3.us-west-1.amazonaws.com/v2/49/assets/src/react-native-logo.png"
+        );
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   // Effect to prevent auto-hiding of splash screen initially
   useEffect(() => {
