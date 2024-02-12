9c96218e41b20665dfd305cc4d11a5575427f2ad
Vishal Borana <vishal2develop@gmail.com>
2024-02-12 22:42:19 +0530

    Splash screen exercise

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  // Effect to handle app readiness and hide splash screen
+  useEffect(() => {
+    const handleFinish = async () => {
+      // Set the app as ready
+      setReady(true);
+      // Hide the splash screen when the app is ready
+      await SplashScreen.hideAsync();
+    };
+    handleFinish();
+  }, [ready]);
+
+  // Effect to prevent auto-hiding of splash screen initially
+  useEffect(() => {
+    // Prevent auto-hiding of splash screen when the component mounts
+    if (!ready) {
+      SplashScreen.preventAutoHideAsync();
+    }
+  }, []); // Run the effect only once when the component mounts
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>SplashScreen Demo! 👋</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
