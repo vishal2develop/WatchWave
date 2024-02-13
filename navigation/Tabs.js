8b081b64648e43c680da8c24c3ceda9249563951
Vishal Borana <vishal2develop@gmail.com>
2024-02-13 23:27:48 +0530

    customize Tabbar icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,6 +4,8 @@ import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import { Text, View, useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 import {
   BLACK_COLOR,
   BLUE_GRAY,
@@ -26,11 +28,36 @@ const Tabs = () => {
         tabBarInactiveTintColor: isDark ? WHITE_COLOR : BLUE_GRAY,
         headerStyle: { backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR },
         headerTitleStyle: { color: isDark ? YELLOW_COLOR : BLACK_COLOR },
+        tabBarLabelStyle: { fontSize: 12, fontWeight: "600", marginTop: -4 },
       }}
     >
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
