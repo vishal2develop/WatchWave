f4c3522e1320779a25ee8ccb56ef6e75c54d541d
Vishal Borana <vishal2develop@gmail.com>
2024-03-18 18:15:41 +0530

    Showing Movie Details on Movie Screen, Opening Video in youtube using expo-web-browser, Showing list of youtube links

--- api.ts
+++ api.ts
@@ -2,13 +2,6 @@ import { QueryFunction } from "@tanstack/react-query";
 
 const API_READ_ACCESS_TOKEN = process.env.API_READ_ACCESS_TOKEN;
 
-interface BaseResponse {
-  page: number;
-  total_results: number;
-  total_pages: number;
-  dates: { maximum: string; minimum: string };
-}
-
 export interface Movie {
   adult: boolean;
   backdrop_path: string | null;
@@ -24,6 +17,42 @@ export interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: object;
+  budget: number;
+  genres: object;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object;
+  production_countries: object;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: object;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  videos: {
+    results: {
+      name: string;
+      key: string;
+      site: string;
+    }[];
+  };
+  images: object;
 }
 
 export interface TV {
@@ -43,6 +72,55 @@ export interface TV {
   media_type: string;
 }
 
+export interface TVDetails {
+  backdrop_path: string;
+  created_by: object;
+  episode_run_time: object;
+  first_air_date: string;
+  genres: object;
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: object;
+  last_air_date: string;
+  last_episode_to_air: object;
+  name: string;
+  next_episode_to_air: object;
+  networks: object;
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: object;
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object;
+  production_countries: object;
+  seasons: object;
+  spoken_languages: object;
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+  videos: {
+    results: {
+      name: string;
+      key: string;
+      site: string;
+    }[];
+  };
+  images: object;
+}
+
+interface BaseResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  dates: { maximum: string; minimum: string };
+}
+
 export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
@@ -51,8 +129,23 @@ export interface TVResponse extends BaseResponse {
   results: TV[];
 }
 
-interface Fetchers<T> {
-  [key: string]: QueryFunction<T>;
+type MovieListResponse = QueryFunction<MovieResponse>;
+type TVListResponse = QueryFunction<TVResponse>;
+
+interface MovieFetchers {
+  trending: MovieListResponse;
+  upcoming: MovieListResponse;
+  nowPlaying: MovieListResponse;
+  search: MovieListResponse;
+  detail: QueryFunction<MovieDetails>;
+}
+
+interface TVFetchers {
+  trending: TVListResponse;
+  airingToday: TVListResponse;
+  topRated: TVListResponse;
+  search: TVListResponse;
+  detail: QueryFunction<TVDetails>;
 }
 
 const BASE_URL = "https://api.themoviedb.org/3";
@@ -65,7 +158,7 @@ const nowPlaying = "";
 
 const upcoming = "";
 
-export const moviesApi: Fetchers<MovieResponse> = {
+export const moviesApi: MovieFetchers = {
   trending: () => {
     const options = {
       method: "GET",
@@ -141,7 +234,7 @@ export const moviesApi: Fetchers<MovieResponse> = {
   },
 };
 
-export const tvApi: Fetchers<TVResponse> = {
+export const tvApi: TVFetchers = {
   trending: () => {
     const options = {
       method: "GET",
--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -2,13 +2,16 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text } from "react-native";
 import React, { useEffect } from "react";
 import styled from "styled-components/native";
-import { Movie, moviesApi, TV, tvApi } from "../api";
+import { Movie, MovieDetails, moviesApi, TV, TVDetails, tvApi } from "../api";
 import Poster from "../components/Poster";
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Linking } from "react-native";
 import { makeImagePath } from "../utils";
 import { LinearGradient } from "expo-linear-gradient";
 import { BLACK_COLOR } from "../constants/colors";
 import { useQuery } from "@tanstack/react-query";
+import Loader from "../components/Loader";
+import { Ionicons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -35,10 +38,26 @@ const Title = styled.Text`
   margin-left: 15px;
   font-weight: 500;
 `;
+
+const Data = styled.View`
+  padding: 0px 20px;
+`;
+
 const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
   margin-top: 20px;
   padding: 0px 20px;
+`;
+
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
 `;
 
 type RootStackParamList = {
@@ -51,25 +70,24 @@ const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
-  const { isLoading: moviesLoading, data: moviesData } = useQuery({
-    queryKey: ["movies", params.id],
-    queryFn: moviesApi.detail,
-    enabled: "original_title" in params,
-  });
+  const isMovie = "original_title" in params;
 
-  const { isLoading: tvLoading, data: tvData } = useQuery({
-    queryKey: ["tv", params.id],
-    queryFn: tvApi.detail,
-    enabled: "original_name" in params,
+  const { isLoading, data } = useQuery<MovieDetails | TVDetails>({
+    queryKey: [isMovie ? "movies" : "tv", params.id],
+    queryFn: isMovie ? moviesApi.detail : tvApi.detail,
   });
-  console.log("movies", moviesData);
-  console.log("tv", tvData);
 
   useEffect(() => {
     setOptions({
       title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
+
+  const openYTLink = async (videoID: string) => {
+    const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
+    // await Linking.openURL(baseUrl);
+    await WebBrowser.openBrowserAsync(baseUrl);
+  };
 
   return (
     <Container>
@@ -91,7 +109,19 @@ const Detail: React.FC<DetailScreenProps> = ({
           </Title>
         </Column>
       </Header>
-      <Overview>{params.overview}</Overview>
+      <Data>
+        <Overview>{params.overview}</Overview>
+        {isLoading ? <Loader /> : null}
+        {data?.videos?.results?.map((video) =>
+          video.site === "YouTube" ? (
+            <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+              <Ionicons name="logo-youtube" color="white" size={24} />
+              <BtnText>{video.name}</BtnText>
+            </VideoBtn>
+          ) : null
+        )}
+      </Data>
+      {/* <Overview>{params.overview}</Overview> */}
     </Container>
   );
 };
